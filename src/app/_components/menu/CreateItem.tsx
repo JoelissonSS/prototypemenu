0241de7edf20b39e778081a47d9188a2c304baa4
Joélisson Santana <joelisson997@gmail.com>
2024-06-13 11:24:42 -0300

    feat: adicionado toast and type e schema para o form e o error

--- src/app/_components/menu/CreateItem.tsx
+++ src/app/_components/menu/CreateItem.tsx
@@ -1,23 +1,89 @@
 "use client";
 import { api } from "@/trpc/react";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ZodError, getErrorMap, z } from "zod";
+import { type categoryType } from "@/server/api/routers/menu";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
+import { type TRPCClientErrorLike } from "@trpc/client";
+import { type AppRouter } from "@/server/api/root";
+import errorMap from "zod/locales/en.js";
 
 const CreateItem = () => {
   const router = useRouter();
-  const [, setName] = React.useState("");
+  const toast = useToast();
 
   const createItem = api.menu.createItem.useMutation({
     onSuccess: () => {
       router.refresh();
-    }
-  })
-  
+      toast.toast({
+        title: "Item criado com sucesso",
+        status: "success",
+      });
+    },
+    onError: (error: TRPCClientErrorLike<AppRouter>) => {
+      toast.toast({
+        title: error.message,
+      });
+    },
+  });
 
-  return <form onClick={(e) => {
-    e.preventDefault()
-    setN
-  }}></form>;
+  type CreateItemsSchema = z.infer<typeof createItemSchema>;
+
+  const createItemSchema = z.object({
+    name: z.string().min(4, {message:'o campo de nome deve ser preechido  '}),
+    description: z.string(),
+    category: z.custom<categoryType>(),
+  });
+
+
+  const { register, handleSubmit,formState, formState: {errors}, resetField, reset } =
+    useForm<CreateItemsSchema>({
+      resolver: zodResolver(createItemSchema),
+    });
+
+  function handleCreateItem(data: CreateItemsSchema) {
+    const result = createItemSchema.safeParse({data})
+    
+    const { name, description, category } = data;
+
+    createItem.mutate({
+      name,
+      description,
+      category,
+    });
+  }
+  onerror
+
+  React.useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      resetField("name");
+      resetField("description");
+      resetField("category");
+      reset();
+    }
+  }, [formState, reset, resetField]);
+
+  return (
+    <form onSubmit={handleSubmit(handleCreateItem, onerror)}>
+      <Input placeholder="Insira o nome do item aqui" {...register("name")} /> 
+      <Input
+        placeholder="Insira a descrição do item"
+        {...register("description")}
+      />
+      <Input
+        placeholder="Insira o nome da categoria aqui"
+        {...register("category")}
+      />
+      <Button>Crie o item</Button>
+      <Toaster />
+    </form>
+  );
 };
 
 export default CreateItem;
--- src/server/api/routers/menu.ts
+++ src/server/api/routers/menu.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-type categoryType = "hambuguer" | "drink" | "portions"
+import { TRPCError } from "@trpc/server";
+export type categoryType = "hambuguer" | "drink" | "portions";
 export const menuRouter = createTRPCRouter({
   createItem: publicProcedure
     .input(
@@ -12,29 +13,24 @@ export const menuRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ input, ctx }) => {
-        
-             return await ctx.db.item.create({
-              data: {
-                name: input.name,
-                description: input.description,
-                category: input.category,
-              
-             }});
-        }),
+      const existingItem = await ctx.db.item.findUnique({
+        where: { name: input.name },
+      });
+      if (existingItem) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Item já existe",
+        });
+      }
+      return await ctx.db.item.create({
+        data: {
+          name: input.name,
+          description: input.description,
+          category: input.category,
+        },
+      });
+    }),
   getItems: publicProcedure.query(async ({ ctx }) => {
     return await ctx.db.item.findMany();
   }),
-  deleteItem: publicProcedure
-   .input(
-      z.object({
-        name: z.string(),
-      }),
-    )
-    .mutation(async({input, ctx}) => await ctx.db.item.deleteMany({
-      where: {
-        name: input.name,
-      },
-    }))
-    
-    
-})
+});
